4569ce081caf9e0de19a511838e8d018320d1cd7
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-04-21 14:20:15 -0700

    finish stretch goal

--- javascript/code-challenges/arrayBinarySearch/__tests__/array-binary-search.test.js
+++ javascript/code-challenges/arrayBinarySearch/__tests__/array-binary-search.test.js
@@ -1,6 +1,10 @@
 'use strict'
 
 const arrBinarySearch = require('../array-binary-search')
+let bigArr = []
+for (var i = 0; i < 10000000; i++) {
+  bigArr.push(i)
+}
 
 describe('ARRAY BINARY SEARCH:', ()=>{
     it('test 1', ()=>{
@@ -10,6 +14,6 @@ describe('ARRAY BINARY SEARCH:', ()=>{
         expect(arrBinarySearch([11,22,33,44,55,66,77], 90)).toStrictEqual(-1)
     })
     it('test 3', ()=>{
-        expect(arrBinarySearch([1, 2, 3, 5, 6, 7], 4)).toStrictEqual(-1)
+        expect(arrBinarySearch(bigArr, 8)).toStrictEqual(8)
     })
 })
